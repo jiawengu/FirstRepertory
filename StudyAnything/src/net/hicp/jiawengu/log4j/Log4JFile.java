ae064d6c8e3b5da2a813b8ea027920c3f9f13370
jiawengu <guxiagufei@163.com>
2015-12-07 10:22:21 +0800

    不知道

--- StudyAnything/src/net/hicp/jiawengu/log4j/Log4JFile.java
+++ StudyAnything/src/net/hicp/jiawengu/log4j/Log4JFile.java
@@ -1,12 +1,12 @@
 package net.hicp.jiawengu.log4j;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class Log4JFile {
-	static Logger log = Logger.getLogger(Log4JFile.class);
+	static Log log = LogFactory.getLog(Log4JFile.class);
 	public static void main(String[] args) {
-		PropertyConfigurator.configure("log4j/log4j.properties");
+//		PropertyConfigurator.configure("log4j.properties");
 		log.info("hello");
 		log.debug("hello");
 	}
